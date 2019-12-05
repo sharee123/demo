02c8cc3ff8ed5a21f47a8bce2b3b2927076ed61a
sharee123 <shareenair15@gmail.com>
2019-12-05 10:47:56 +0530

    commit to bitbucket

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_04_123801) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
